229ce79e59fef8c23471a282e4955ece3b3a8457
Thomas Rückert <thomas.rueckert@s2010.tu-chemnitz.de>
2015-07-01 11:47:16 +0200

    ue9 init and a1,a2,a3

---
+++ Uebung9/integral.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<math.h>
+#include<omp.h>
+#include<immintrin.h>
+
+#define HOST 	0
+#define MIC 	1
+
+__attribute__((target(mic)))
+double calculateArea (double a, double b, unsigned long long N) 
+{
+	double dx = (b - a) / (double) N;
+	double area = 0;
+	//custom
+	double sum = 0;
+ 	double x1 = 0;
+ 	double x2 = 0;
+	double y1 = 0;
+ 	double y2 = 0;
+
+	#ifdef __MIC__
+		// FIXME 3. u. 5. Aufgabe
+	#else
+		area = ((b-a)/(2*N));
+		
+		for (int i = 0; i < N; i++) {
+		    
+		    x1 = (i*((b-a)/N)) + a;
+		    x2 = ((i+1)*((b-a)/N)) + a;
+		    
+		    y1 = pow(x1, 3) - 2*(pow(x1, 2)) + 3;
+		    y2 = pow(x2, 3) - 2*(pow(x2, 2)) + 3;
+		    
+		    sum += (y1+y2);
+		}
+		pow(x1, 3) - 2*(pow(x1, 2)) + 3;
+		area *= sum;
+	#endif
+
+	return area;
+}
+
+int main() 
+{
+	unsigned long long N = (10000000000 / 8) * 8; // 10.000.000.000
+	double a = -1.0;
+	double b = +2.8;
+	double dx = (b - a) / (double) N;
+	double area = -1;
+	int where = HOST;
+
+	printf("a: \t %lf\n", a);
+	printf("b: \t %lf\n", b);
+	printf("dx: \t %.14lf\n", dx);
+
+	double startTime = omp_get_wtime();
+
+	#pragma offload target(mic) in(a, b, N) out(area, where) // FIXME mic:<id>
+	{
+		#ifdef __MIC__
+			where = MIC;
+		#else
+			where = HOST;
+		#endif
+
+		area = calculateArea(a, b, N);
+	}
+
+	double endTime = omp_get_wtime();
+
+	printf("area: \t %.14lf\n", area);
+	printf("time: \t %.14lf\n", endTime - startTime);
+	printf("where: \t ");
+
+	if (where == MIC) 
+		printf("MIC\n");
+	else
+		printf("HOST\n");
+	
+	return 0;
+}
